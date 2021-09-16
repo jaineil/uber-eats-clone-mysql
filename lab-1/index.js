76c7b2695fd6cc1a1e2a17d3716dbe78620e812d
jaineil <mandaviajaineil@gmail.com>
2021-09-16 00:48:00 -0700

    feat: setup remote connection to rds-mysql

--- lab-1/index.js
+++ lab-1/index.js
@@ -1,1 +1,13 @@
-console.log("Welcome, Jaineil");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+	host: "uber-eats-clone.ckjsjavkgqyy.us-east-2.rds.amazonaws.com",
+	user: "admin",
+	password: "cmpe273_lab",
+	database: "uber-eats",
+});
+
+connection.connect((err) => {
+	if (err) throw err;
+	console.log("Connected!");
+});
